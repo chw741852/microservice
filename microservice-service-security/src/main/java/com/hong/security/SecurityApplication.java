28d170ebb0d9dafc3b0978591e1963b1a9e1c36b
caihongwei <caihongwei@mochui.com>
2016-07-21 15:50:06 +0800

    添加spring session redis

--- microservice-service-security/src/main/java/com/hong/security/SecurityApplication.java
+++ microservice-service-security/src/main/java/com/hong/security/SecurityApplication.java
@@ -3,12 +3,14 @@ package com.hong.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by caihongwei on 16/6/20 下午5:05.
  */
 @SpringBootApplication(scanBasePackages = "com.hong.security")
 //@EnableEurekaClient
+//@EnableRedisHttpSession
 public class SecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
--- microservice-service-security/src/main/java/com/hong/security/controller/SecurityController.java
+++ microservice-service-security/src/main/java/com/hong/security/controller/SecurityController.java
@@ -1,8 +1,11 @@
 package com.hong.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by caihongwei on 16/6/28 下午2:02.
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SecurityController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
+    public String index(ModelMap modelMap, HttpServletRequest request) {
+        modelMap.addAttribute("sessionId", request.getSession().getId());
+
         return "index";
     }
 }
