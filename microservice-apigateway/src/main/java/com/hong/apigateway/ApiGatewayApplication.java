0a16e9aa64c2903b243d004309dbd54b4ed7e85a
caihongwei <caihongwei@mochui.com>
2016-07-27 15:22:06 +0800

    update config files

--- microservice-apigateway/src/main/java/com/hong/apigateway/ApiGatewayApplication.java
+++ microservice-apigateway/src/main/java/com/hong/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,29 @@
 package com.hong.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by caihongwei on 16/4/1 上午10:31.
  */
 @SpringBootApplication
 @EnableSidecar
+@RestController
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Value("${test.info:default}")
+    private String info;
+
+    @RequestMapping("/test")
+    public String test() {
+        return info;
     }
 }
